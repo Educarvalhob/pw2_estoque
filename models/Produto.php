8a76342a163a8ec17a67df0049d932630f75477d
Educarvalhob <edumengocarvalho@gmail.com>
2024-03-01 21:55:38 -0300

    First commit

---
+++ models/Produto.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once "Categoria.php";
+
+class Produto{
+    private $id;
+    private $nome;
+    private $descricao;
+    private $categoria;
+    private $preco;
+
+    function __construct(
+        $id,
+        $nome,
+        $descricao,
+        Categoria $categoria,
+        $preco,
+    ){
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->descricao = $descricao;
+        $this->categoria = $categoria;
+        $this->preco = $preco;
+    }
+
+    function getId(){
+        return $this->id;
+    }
+    
+    function setId($id){
+        $this->id = $id;
+    }
+
+    function getNome(){
+        return $this->Nome;
+    }
+    
+    function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    function getCategoria(){
+        return $this->categoria;
+    }
+    
+    function setCategoria(Categoria $categoria){
+        $this->categoria = $categoria;
+    }
+
+    function getDescricao(){
+        return $this->descricao;
+    }
+    
+    function setDescricao($descricao){
+        $this->descricao = $descricao;
+    }
+
+    function getPreco(){
+        return $this->preco;
+    }
+    
+    function setPreco($preco){
+        $this->preco = $preco;
+    }
+}
