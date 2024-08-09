8ce2ec4336fa7115aea2b25a7527c20482d6ee6a
Educarvalhob <edumengocarvalho@gmail.com>
2024-08-09 15:01:08 -0300

    atualização views

---
+++ views/delete_categoria.php
@@ -0,0 +1,13 @@
+<?php
+require_once "controllers/CategoriaController.php";
+
+if (isset($_GET["id"])) {
+    $categoriaController = new CategoriaController();
+    $categoriaController->delete($_GET["id"]);
+
+    // Voltando pra tela anterior
+    // header("Location: ?pg=categorias");
+    echo '<script type="text/javascript">
+             window.location = "?pg=categorias";
+          </script>';
+}
